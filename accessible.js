ad25bccb770ff5a20d7d14cd859a1e960be65d66
Brian Grinstead <briangrinstead@gmail.com>
2013-08-20 15:36:17 -0500

    add stress.html as a way for testing larger inputs

--- accessible.js
+++ accessible.js
@@ -1,56 +1,65 @@
 window.onload=function(){
-
-var myTextArea = document.getElementById("editor");
-var textareaEditor = document.getElementById('textarea-editor');
-textareaEditor.value = myTextArea.value;
-var clonedTextArea = myTextArea.cloneNode();
-
-clonedTextArea.id = "editor2";
-document.getElementById("original-demo").appendChild(clonedTextArea);
-var myScreenreader = document.getElementById("editor-screenreader");
-
-var myContentEditable = document.getElementById("content-editable");
-var button = document.getElementById("toggle-textarea-visibility");
-
-button.onclick = toggleTextareaDisplay;
-
-var editor = window.editor = CodeMirror.fromTextArea(myTextArea, {
-        lineNumbers: true,
-        matchBrackets: true,
-        continueComments: "Enter",
-        extraKeys: {"Ctrl-Q": "toggleComment"}
-});
-
-var editor2 = window.editor2 = CodeMirrorOriginal.fromTextArea(clonedTextArea, {
-        lineNumbers: true,
-        matchBrackets: true,
-        continueComments: "Enter",
-        extraKeys: {"Ctrl-Q": "toggleComment"}
-});
-
-var textareaVisible = false;
-function toggleTextareaDisplay() {
-
-    if (!editor.display.input.getAttribute("backup-style")) {
-        editor.display.input.setAttribute("backup-style", editor.display.input.getAttribute("style"));
-        editor.display.input.parentNode.setAttribute("backup-style", editor.display.input.parentNode.getAttribute("style"));
+    if (!window.waitToLoad) {
+        load();
     }
-
-    if (textareaVisible) {
-        editor.display.input.setAttribute("style", editor.display.input.getAttribute("backup-style"));
-        editor.display.input.parentNode.setAttribute("style", editor.display.input.parentNode.getAttribute("backup-style"));
-        editor2.display.input.setAttribute("style", editor.display.input.getAttribute("backup-style"));
-        editor2.display.input.parentNode.setAttribute("style", editor.display.input.parentNode.getAttribute("backup-style"));
-    }
-    else {
-        editor.display.input.setAttribute("style", "height: 100px; width: 95%; margin-left: 3%; ");
-        editor.display.input.parentNode.setAttribute("style", "");
-        editor2.display.input.setAttribute("style", "height: 100px; width: 95%; margin-left: 3%; ");
-        editor2.display.input.parentNode.setAttribute("style", "");
-    }
-
-    textareaVisible = !textareaVisible;
-}
-
-
 };
+
+function load() {
+    if (window.loaded) {
+        return;
+    }
+    window.loaded = true;
+
+    var myTextArea = document.getElementById("editor");
+    var textareaEditor = document.getElementById('textarea-editor');
+    var clonedTextArea = document.getElementById('editor2');
+
+    textareaEditor.value = clonedTextArea.value = myTextArea.value;
+
+    document.getElementById("original-demo").appendChild(clonedTextArea);
+    var myScreenreader = document.getElementById("editor-screenreader");
+
+    var myContentEditable = document.getElementById("content-editable");
+    var button = document.getElementById("toggle-textarea-visibility");
+
+    button.onclick = toggleTextareaDisplay;
+
+    var editor = window.editor = CodeMirror.fromTextArea(myTextArea, {
+            lineNumbers: true,
+            matchBrackets: true,
+            continueComments: "Enter",
+            extraKeys: {"Ctrl-Q": "toggleComment"}
+    });
+
+    var editor2 = window.editor2 = CodeMirrorOriginal.fromTextArea(clonedTextArea, {
+            lineNumbers: true,
+            matchBrackets: true,
+            continueComments: "Enter",
+            extraKeys: {"Ctrl-Q": "toggleComment"}
+    });
+
+    var textareaVisible = false;
+    function toggleTextareaDisplay() {
+
+        if (!editor.display.input.getAttribute("backup-style")) {
+            editor.display.input.setAttribute("backup-style", editor.display.input.getAttribute("style"));
+            editor.display.input.parentNode.setAttribute("backup-style", editor.display.input.parentNode.getAttribute("style"));
+        }
+
+        if (textareaVisible) {
+            editor.display.input.setAttribute("style", editor.display.input.getAttribute("backup-style"));
+            editor.display.input.parentNode.setAttribute("style", editor.display.input.parentNode.getAttribute("backup-style"));
+            editor2.display.input.setAttribute("style", editor.display.input.getAttribute("backup-style"));
+            editor2.display.input.parentNode.setAttribute("style", editor.display.input.parentNode.getAttribute("backup-style"));
+        }
+        else {
+            editor.display.input.setAttribute("style", "height: 100px; width: 95%; margin-left: 3%; ");
+            editor.display.input.parentNode.setAttribute("style", "");
+            editor2.display.input.setAttribute("style", "height: 100px; width: 95%; margin-left: 3%; ");
+            editor2.display.input.parentNode.setAttribute("style", "");
+        }
+
+        textareaVisible = !textareaVisible;
+    }
+
+}
